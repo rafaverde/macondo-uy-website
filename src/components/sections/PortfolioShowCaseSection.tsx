9d5880da43c7ca44fd3a2e9de95e8e1bd2f71a22
Rafael Valverde <rafaverde@msn.com>
2025-07-28 13:24:48 -0300

    refactor: Change the way portfolio gets data to filter by productId when it exists. Create see more button.

--- src/components/sections/PortfolioShowCaseSection.tsx
+++ src/components/sections/PortfolioShowCaseSection.tsx
@@ -1,29 +1,30 @@
 import {
   GET_LATEST_PORTFOLIO_CASES,
-  GET_PORTFOLIO_CASES_BY_CATEGORY,
+  GET_PORTFOLIO_CASES_BY_PRODUCT_ID,
 } from "@/graphql/queries";
 import { client } from "@/lib/apollo";
 import { PortifolioCase } from "@/lib/types";
 import { PortfolioSlider } from "../PortfolioSlider";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export async function PortfolioShowCaseSection({
-  categoryName,
+  productId,
+  categoryTitle,
 }: {
-  categoryName?: string;
+  productId?: string;
+  categoryTitle?: string;
 }) {
   let portfolioCases: PortifolioCase[] = [];
-  let displayCategoryName = "";
 
-  if (categoryName) {
+  if (productId) {
     const { data } = await client.query({
-      query: GET_PORTFOLIO_CASES_BY_CATEGORY,
-      variables: { categoryName },
+      query: GET_PORTFOLIO_CASES_BY_PRODUCT_ID,
+      variables: { productId },
     });
 
-    const categoryData = data.portfolioCategories?.nodes[0];
-
-    portfolioCases = categoryData.portfolios?.nodes || [];
-    displayCategoryName = categoryData?.name || categoryName;
+    portfolioCases = data.portfolios?.nodes || [];
   } else {
     const { data } = await client.query({
       query: GET_LATEST_PORTFOLIO_CASES,
@@ -36,17 +37,15 @@ export async function PortfolioShowCaseSection({
     return null;
   }
 
-  console.log(portfolioCases);
-
   return (
-    <section className="bg-background w-full scroll-mt-[50px] pt-10 pb-0">
+    <section className="bg-background w-full scroll-mt-[50px] py-10">
       <div className="container mx-auto p-4">
         <h2 className="text-primary text-center text-2xl font-bold md:text-4xl">
-          {categoryName
-            ? `Conozca nuestro trabajo de ${displayCategoryName}`
+          {categoryTitle
+            ? `Conozca nuestro trabajo de ${categoryTitle}`
             : "Portfolio y cases"}
         </h2>
-        {!categoryName && (
+        {!categoryTitle && (
           <p className="text-foreground mt-5 text-center">
             Conocé algunos de los trabajos que desarrollamos para marcas reales
             con desafíos únicos. Desde estrategias digitales hasta identidades
@@ -56,6 +55,13 @@ export async function PortfolioShowCaseSection({
         )}
 
         <PortfolioSlider portfolioCases={portfolioCases} />
+        <div className="mt-8 w-full text-center">
+          <Button asChild size="xl">
+            <Link href="/portfolio">
+              Hay más ejemplos <ArrowRight />
+            </Link>
+          </Button>
+        </div>
       </div>
     </section>
   );
