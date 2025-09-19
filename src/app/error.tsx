80b12462289b13cc516ed7684364fb5d945aa591
Rafael Valverde <rafaverde@msn.com>
2025-09-19 10:37:40 -0300

    style: error page layout fixed.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import * as Sentry from "@sentry/nextjs";
-import Image from "next/image";
 import { useEffect } from "react";
-import notFoundImage from "@/assets/404.webp";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { RefreshCcw } from "lucide-react";
 
 export default function Error({
@@ -23,16 +20,7 @@ export default function Error({
 
   return (
     <section id="paraquien" className="bg-background w-full scroll-mt-[50px]">
-      <div className="container mx-auto grid max-h-[calc(100vh-70px)] items-center justify-center gap-8 p-4 md:grid-cols-2">
-        <div>
-          <Image
-            src={notFoundImage}
-            alt="Erro 404, No se pudo cargar esta página."
-            width={700}
-            height={700}
-          />
-        </div>
-
+      <div className="container mx-auto grid h-[calc(100vh-70px)] items-center justify-center gap-8 p-4">
         <div className="space-y-5">
           <h2 className="text-primary text-7xl font-bold">
             Ups. <br /> Algo salió mal.
@@ -41,10 +29,8 @@ export default function Error({
             No se pudo cargar esta página. Inténtalo de nuevo.
           </p>
 
-          <Button asChild size="xl" className="mt-8" onClick={() => reset()}>
-            <Link href="/">
-              <RefreshCcw /> Intentar otra vez
-            </Link>
+          <Button size="xl" className="mt-8" onClick={() => reset()}>
+            <RefreshCcw /> Intentar otra vez
           </Button>
         </div>
       </div>
