36c23d31029c90522f21850fd82be124915f1a29
Rafael Valverde <rafaverde@msn.com>
2025-10-16 14:57:23 -0300

    feat: enhance getAllTeamMembers function to shuffle team members before returning

--- src/lib/data/teamMembers.ts
+++ src/lib/data/teamMembers.ts
@@ -1,15 +1,24 @@
 import { cache } from "react";
 import { client } from "../apollo";
-import { TeamMemberResponse } from "@/types";
+import { TeamMember, TeamMemberResponse } from "@/types";
 import { GET_ALL_TEAM_MEMBERS } from "@/graphql";
 
 export const getAllTeamMembers = cache(async () => {
   try {
     const { data } = await client.query<TeamMemberResponse>({
       query: GET_ALL_TEAM_MEMBERS,
+      fetchPolicy: "no-cache",
     });
 
-    return data.teamMembers.nodes;
+    let teamMembers: TeamMember[] = data.teamMembers.nodes;
+
+    for (let i = teamMembers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [teamMembers[i], teamMembers[j]] = [teamMembers[j], teamMembers[i]];
+    }
+
+    return teamMembers;
   } catch (error) {
     console.error("Erro ao buscar equipe:", error);
     throw new Error("Não foi possível carregar os membros da equipe.");
